834845d403ba650a5da4f8bc8957d3d63a65dafb
Taylor1990 <has728975@yandex.ru>
2015-11-26 10:39:51 +0300

    Added right_menu

--- components/Panel.jsx
+++ components/Panel.jsx
@@ -1,9 +1,11 @@
-var React = require('react');
+var React = require('react'),
+    RightMenu = require('./RightMenu.jsx');
 
 var Panel = React.createClass({
     render: function() {
         return (
             <div id="panel" className="panel">
+                <RightMenu className="unvisible"/>
             </div>
         );
     }
---
+++ components/RightMenu.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+var RightMenu = React.createClass({
+    getInitialState: function() {
+        return {className: 'unvisible'};
+    },
+    openMenu : function() {
+        this.state.className == 'visible' ? this.setState({className: 'unvisible'}) : this.setState({className: 'visible'});
+
+    },
+    render: function() {
+       return (
+         <div className="menu__right" onClick={this.openMenu}>
+             Menu &#8595;
+             <div className = {this.state.className}>
+                 <ul>
+                     <li>Profile</li>
+                     <li>Exit</li>
+                 </ul>
+             </div>
+
+         </div>
+       );
+    }
+});
+
+module.exports = RightMenu;
