919d0ddb87079a13bb60b8cde36537fa2f8fad12
Denis Raslov <d.raslov@neemble.ru>
2016-03-03 18:05:08 +0300

    Task name/description editing fixes

--- components/PopupTask.jsx
+++ components/PopupTask.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Dialog from 'material-ui/lib/dialog';
 import FlatButton from 'material-ui/lib/flat-button';
 import RaisedButton from 'material-ui/lib/raised-button';
@@ -24,22 +25,14 @@ export default class extends React.Component {
     }
 
     editTitle(e) {
-        let value = e.target.value;
-
-        this.setState({openEditTitle: !this.state.openEditTitle}, () => {
-            this.state.openEditTitle ?
-                document.getElementById('editTitle').focus() :
-                this.props.changeTaskName(value, this.state.model.id);
+        this.setState({openEditTitle: true}, () => {
+            ReactDOM.findDOMNode(this.refs.titleInput).getElementsByTagName('input')[0].focus();
         });
     }
 
     editDescription(e) {
-        let value = e.target.value;
-
-        this.setState({openEditDescription: !this.state.openEditDescription}, () => {
-            this.state.openEditDescription ?
-                document.getElementById('editDescription').focus() :
-                this.props.changeTaskDescription(value, this.state.model.id);
+        this.setState({openEditDescription: true }, () => {
+            ReactDOM.findDOMNode(this.refs.descriptionInput).focus();
         });
     }
 
@@ -58,44 +51,51 @@ export default class extends React.Component {
                 secondary={true}
                 onTouchTap={this.props.onRequestClose}
                 />
-        ]
+        ];
+
         return (
             <Dialog
-                title={
-                    this.state.openEditTitle?
-                        <TextField
-                            id='editTitle'
-                            className='taskPopup__title___enabled'
-                            onEnterKeyDown={this.editTitle.bind(this)}
-                            hintText={this.state.model.name}
-                            onBlur={this.lostFocusTitle.bind(this)}
-                            />:
-                        <div
-                            className='taskPopup__title___disabled'
-                            onClick={this.editTitle.bind(this)}>
-                            {this.state.model.name}
-                        </div>
-                }
                 actions={actions}
                 modal={false}
                 open={this.props.open}
                 onRequestClose={this.props.onRequestClose}
-                >
+            >
                 {
-                    this.state.openEditDescription ?
+                    this.state.openEditTitle ?
                         <TextField
-                            id='editDescription'
-                            className='taskPopup__description'
-                            onEnterKeyDown={this.editDescription.bind(this)}
-                            hintText={this.state.model.description}
-                            onBlur={this.lostFocusDescription.bind(this)}
-                            /> :
+                            id='title'
+                            ref="titleInput"
+                            className='taskPopup__titleInput'
+                            onEnterKeyDown={this.editTitle.bind(this)}
+                            readOnly={false}
+                            value={this.state.model.name}
+                            onBlur={this.lostFocusTitle.bind(this)}
+
+                        /> :
                         <div
-                            className='taskPopup__description'
-                            onClick={this.editDescription.bind(this)}>
-                            {this.state.model.description}
+                            className='taskPopup__title'
+                            onClick={this.editTitle.bind(this)}>
+                            {this.state.model.name}
                         </div>
                 }
+
+
+                {this.state.openEditDescription ?
+                    <textarea
+                        id='description'
+                        ref="descriptionInput"
+                        className='taskPopup__descriptionInput'
+                        onEnterKeyDown={this.editDescription.bind(this)}
+                        value={this.state.model.description}
+                        onBlur={this.lostFocusDescription.bind(this)}
+                        /> :
+                    <div
+                        className='taskPopup__description'
+                        onClick={this.editDescription.bind(this)}>
+                        {this.state.model.description}
+                    </div>
+                }
+
                 <Checkbox checked={this.state.model.completed}
                           onCheck={this.changeTaskStatus.bind(this)}
                           className='taskPopup__checkbox'
