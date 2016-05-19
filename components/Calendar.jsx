432816df223d41d503e7a00056dba6a53a88ef23
Taylor1990 <has728975@yandex.ru>
2016-05-19 10:23:21 +0300

    Add scrolling calendar

--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -1,33 +1,76 @@
-import React, {Component} from 'react';
+import React, {
+    Component
+} from 'react';
 import moment from 'moment';
 import CalendarCell from './../components/CalendarCell.jsx';
 
 export default class extends Component {
     constructor(props) {
         super(props);
+
+        const beginMonth = moment().subtract(1, 'months');
+
         this.state = {
-            month: 10,
-            year: 2015,
-            monthesCount: 5,
+            month: beginMonth.month(),
+            year: beginMonth.year(),
+            monthesCount: 4,
             showMonthNames: false
         }
     }
 
+    componentDidMount() {
+        const calendar = document.getElementById('calendar');
+        calendar.scrollTop = 400;
+    }
+
     onScroll(e) {
-        this.setState({showMonthNames: true});
+        if (calendar.scrollHeight == calendar.scrollTop + calendar.clientHeight) {
+            let beginMonth = moment({
+                M: this.state.month,
+                y: this.state.year
+            }).add(1, 'months');
+            this.setState({
+                showMonthNames: true,
+                month: beginMonth.month(),
+                year: beginMonth.year()
+            }, () => {
+                const calendar = document.getElementById('calendar');
+                calendar.scrollTop -= 400;
+            });
+        } else if (calendar.scrollTop == 0) {
+            let beginMonth = moment({
+                M: this.state.month,
+                y: this.state.year
+            }).subtract(1, 'months');
+            this.setState({
+                showMonthNames: true,
+                month: beginMonth.month(),
+                year: beginMonth.year()
+            }, () => {
+                const calendar = document.getElementById('calendar')
+                calendar.scrollTop += 400;
+            });
+        } else {
+            this.setState({
+                showMonthNames: true
+            });
+        }
 
         clearTimeout(this.showMonthNamesTimeout);
         this.showMonthNamesTimeout = setTimeout(() => {
-            this.setState({showMonthNames: false});
+            this.setState({
+                showMonthNames: false
+            });
         }, 600);
     }
 
     getInitialMonthes() {
         let rows = [];
 
-        for (let i = 0, indexMoment = moment({M: this.state.month, y: this.state.year});
-             i < this.state.monthesCount;
-             i++, indexMoment.add(1, 'month')) {
+        for (let i = 0, indexMoment = moment({
+                M: this.state.month,
+                y: this.state.year
+            }); i < this.state.monthesCount; i++, indexMoment.add(1, 'month')) {
             rows = rows.concat(this.getMonth(indexMoment.month(), indexMoment.year()));
         }
 
@@ -35,15 +78,23 @@ export default class extends Component {
     }
 
     getMonth(month, year) {
-        let startMoment = moment({M: month, y: year}).startOf('week'),
-            nextMonth = moment({M: month, y: year}).add(1, 'month').month(),
+        let startMoment = moment({
+                M: month,
+                y: year
+            }).startOf('week'),
+            nextMonth = moment({
+                M: month,
+                y: year
+            }).add(1, 'month').month(),
             indexMoment = startMoment.clone(),
             rows = [],
             cells = [];
 
         while (indexMoment.month() != nextMonth) {
 
-            cells.push({moment: indexMoment.clone()});
+            cells.push({
+                moment: indexMoment.clone()
+            });
 
             if (indexMoment.day() == 0) {
                 rows.push(cells);
@@ -57,7 +108,10 @@ export default class extends Component {
             rows.pop();
         }
 
-        rows[0][0].newMonth = {month: month, year: year};
+        rows[0][0].newMonth = {
+            month: month,
+            year: year
+        };
 
         return rows;
     }
@@ -65,45 +119,54 @@ export default class extends Component {
     render() {
         const rows = this.getInitialMonthes();
 
-        return (
-            <div className='calendar'>
-                <table className='calendar__head'>
-                    <tbody>
-                    <tr>
-                        {
-                            rows[0].map((cell) => {
-                                return <td key={cell.moment.format('D M YYYY')}>{cell.moment.format('dddd')}</td>;
-                            })
-                        }
-                    </tr>
-                    </tbody>
-                </table>
-                <div className='calendar__body' onScroll={this.onScroll.bind(this)}>
-                    <table>
-                        <tbody>
-                        {
-                            rows.map((row, i) => {
-                                return <tr key={'line'+i}>
-                                    {
-                                        row.map((cell) => {
-                                            return (
-                                                <CalendarCell
-                                                    date={cell.moment.format('D M YYYY')}
-                                                    cell={cell}
-                                                    showMonthNames={this.state.showMonthNames}
-                                                    tasks={this.props.tasks}
-                                                    showPopup={this.props.showPopup}
-                                                    changeTask={this.props.changeTask}/>);
-                                        })
-                                    }
-                                </tr>;
-                            })
-                        }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        );
-    }
+        return ( < div className = 'calendar' >
+            < table className = 'calendar__head' >
+            < tbody >
+            < tr > {
+                rows[0].map((cell) => {
+                    return <td key = {
+                        cell.moment.format('D M YYYY')
+                    } > {
+                        cell.moment.format('dddd')
+                    } < /td>;
+                })
+            } < /tr> < /tbody> < /table> < div id = 'calendar'
+            className = 'calendar__body'
+            onScroll = {
+                this.onScroll.bind(this)
+            }
+            scrolling = "yes" >
+            < table >
+            < tbody > {
+                rows.map((row, i) => {
+                        return <tr key = {
+                            'line' + i
+                        } > {
+                            row.map((cell) => {
+                                    return ( < CalendarCell date = {
+                                            cell.moment.format('D M YYYY')
+                                        }
+                                        cell = {
+                                            cell
+                                        }
+                                        showMonthNames = {
+                                            this.state.showMonthNames
+                                        }
+                                        tasks = {
+                                            this.props.tasks
+                                        }
+                                        showPopup = {
+                                            this.props.showPopup
+                                        }
+                                        changeTask = {
+                                            this.props.changeTask
+                                        }
+                                        />);
+                                    })
+                            } < /tr>;
+                        })
+                } < /tbody> < /table> < /div> < /div>
+            );
+        }
 
-}
+    }
